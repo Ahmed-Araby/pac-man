fdedfc0193a8aba44bb0bffe4641744ef0bd6a45
Ahmed Araby <ahmedaraby605@gmail.com>
2026-03-21 23:30:06 +0200

    implement a more accurate canvas to maze coordinate mapping that accounts for the movement direction

--- src/main/java/org/example/util/CoordinateUtil.java
+++ src/main/java/org/example/util/CoordinateUtil.java
@@ -2,6 +2,7 @@ package org.example.util;
 
 import org.example.constant.Dimensions;
 import org.example.constant.DirectionsE;
+import org.example.entity.Coordinate;
 import org.example.entity.MazeCoordinate;
 
 import java.util.ArrayList;
@@ -41,5 +42,28 @@ public class CoordinateUtil {
         } else {
             return DirectionsE.STILL;
         }
+    }
+
+    public static MazeCoordinate toMazeCoordinate(Coordinate cord, DirectionsE dir) {
+        switch (dir) {
+            case RIGHT, DOWN:
+                return toMazeCoordinateFlooring(cord);
+            case LEFT, UP:
+                return toMazeCoordinateCeiling(cord);
+            default:
+                return toMazeCoordinateFlooring(cord);
+        }
+    }
+
+    private static MazeCoordinate toMazeCoordinateFlooring(Coordinate cord) {
+        final int mazeRow = (int) Math.floor(cord.getRow() / Dimensions.CANVAS_CELL_SIZE_PIXELS);
+        final int mazeCol = (int) Math.floor(cord.getCol() / Dimensions.CANVAS_CELL_SIZE_PIXELS);
+        return new MazeCoordinate(mazeRow, mazeCol);
+    }
+
+    private static MazeCoordinate toMazeCoordinateCeiling(Coordinate cord) {
+        final int mazeRow = (int) Math.ceil(cord.getRow() / Dimensions.CANVAS_CELL_SIZE_PIXELS);
+        final int mazeCol = (int) Math.ceil(cord.getCol() / Dimensions.CANVAS_CELL_SIZE_PIXELS);
+        return new MazeCoordinate(mazeRow, mazeCol);
     }
 }
