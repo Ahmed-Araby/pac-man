72a521306e89377f3ef59177c3d3773a8be50e70
Ahmed Araby <ahmedaraby605@gmail.com>
2026-05-18 23:50:10 +0300

    put the ghost mode (state) transition logic into the Ghost abstract class to be resued by all Ghosts

--- src/main/java/org/example/sprite/ghost/Blinky.java
+++ src/main/java/org/example/sprite/ghost/Blinky.java
@@ -5,28 +5,18 @@ import org.example.config.GhostModeActivePeriodsConf;
 import org.example.constant.*;
 import org.example.entity.CanvasCoordinate;
 import org.example.event.Event;
-import org.example.event.EventType;
 import org.example.event.Subscriber;
 import org.example.ghostmode.*;
 import org.example.ghostmode.blinky.BlinkyChaser;
 import org.example.ghostmode.Frightened;
 import org.example.ghostmode.blinky.BlinkyScattered;
-import org.example.ghostmode.timer.ChaseScatterTimer;
 import org.example.ghostmode.timer.RealTimer;
 import org.example.model.GameState;
 
 public class Blinky extends Ghost implements Subscriber {
     // [TODO] rename modes
     // ghost modes
-    private final GhostMode blinkyChaser;
-    private final GhostMode blinkyScattered;
-    private final GhostMode frightened;
-    private final GhostMode eaten;
 
-    private GhostMode activeMode;
-    private GhostMode previousMode;
-    private final ChaseScatterTimer chaseScatterTimer;
-    private final RealTimer realTimer;
 
     private final GameState gameState;
     @Override
@@ -37,15 +27,16 @@ public class Blinky extends Ghost implements Subscriber {
     public Blinky(GameState gameState) {
         super(SpriteE.GHOST, 0, 0, DirectionsE.STILL);
 
-        this.chaseScatterTimer = new ChaseScatterTimer();
         this.realTimer = new RealTimer();
 
         // ghost modes
-        this.blinkyChaser = new BlinkyChaser(GhostModeActivePeriodsConf.LEVEL_1_CHASE_ACTIVE_PERIODS);
-        this.blinkyScattered = new BlinkyScattered(GhostModeActivePeriodsConf.LEVEL_1_SCATTER_ACTIVE_PERIODS);
+        this.chaser = new BlinkyChaser(GhostModeActivePeriodsConf.LEVEL_1_CHASE_ACTIVE_PERIODS);
+        this.scattered = new BlinkyScattered(GhostModeActivePeriodsConf.LEVEL_1_SCATTER_ACTIVE_PERIODS);
         this.frightened = new Frightened();
         this.eaten = new Eaten(gameState);
-        this.activeMode = blinkyScattered;
+
+        scattered.enter();
+        this.activeMode = scattered;
 
         this.gameState = gameState;
     }
@@ -60,48 +51,6 @@ public class Blinky extends Ghost implements Subscriber {
     public void move(Event event) {
         transitionMode(event);
         activeMode.move(this);
-    }
-
-    private void transitionMode(Event event) {
-        GhostMode nextMode = null;
-
-        if (activeMode instanceof Chaser) {
-            if (event != null && EventType.PAC_MAN_SUPER_SUGAR_COLLISION.equals(event.getType())) {
-                previousMode = activeMode;
-                nextMode = frightened;
-                final float activePeriodSeconds = nextMode.getActivePeriodSeconds();
-                realTimer.start(activePeriodSeconds);
-            }
-            else if(chaseScatterTimer.up()) {
-                nextMode = blinkyScattered;
-            }
-        } else if(activeMode instanceof Scattered) {
-            if (event != null && EventType.PAC_MAN_SUPER_SUGAR_COLLISION.equals(event.getType())) {
-                previousMode = activeMode;
-                nextMode = frightened;
-                final float activePeriodSeconds = nextMode.getActivePeriodSeconds();
-                realTimer.start(activePeriodSeconds);
-            }
-            else if(chaseScatterTimer.up()){
-                nextMode = blinkyChaser;
-            }
-        } else if (activeMode instanceof Frightened) {
-            if (event != null && EventType.PAC_MAN_GHOST_COLLISION.equals(event.getType())) {
-                nextMode = eaten;
-            } else if (realTimer.up()) {
-                nextMode = previousMode;
-                previousMode = null;
-            }
-        } else if (activeMode instanceof Eaten) {
-            if (activeMode.end(this)) {
-                nextMode = blinkyChaser;
-            }
-        }
-
-        if(nextMode != null) {
-            activeMode = nextMode;
-            activeMode.enter(this);
-        }
     }
 
     public void update(Event event) {
--- src/main/java/org/example/sprite/ghost/Ghost.java
+++ src/main/java/org/example/sprite/ghost/Ghost.java
@@ -3,13 +3,93 @@ package org.example.sprite.ghost;
 import org.example.constant.DirectionsE;
 import org.example.constant.SpriteE;
 import org.example.entity.CanvasCoordinate;
+import org.example.event.Event;
+import org.example.event.EventType;
+import org.example.ghostmode.*;
+import org.example.ghostmode.timer.RealTimer;
 import org.example.sprite.MovingSprite;
 
 public abstract class Ghost extends MovingSprite {
+    protected TemporalGhostMode scattered;
+    protected TemporalGhostMode chaser;
+    protected GhostMode frightened;
+    protected GhostMode eaten;
+
+    protected GhostMode activeMode;
+    protected TemporalGhostMode previousMode;
+
+    protected RealTimer realTimer;
+
 
     public Ghost(SpriteE type, double col, double row, DirectionsE dir) {
         super(type, col, row, dir);
     }
 
     public abstract CanvasCoordinate getPacManCanvasCord();
+
+
+    protected void transitionMode(Event event) {
+        if (activeMode instanceof Chaser) {
+            chaserTransition(event);
+        } else if(activeMode instanceof Scattered) {
+            scatteredTransition(event);
+        } else if (activeMode instanceof Frightened) {
+            frightenedTransition(event);
+        } else if (activeMode instanceof Eaten) {
+            eatenTransition(activeMode);
+        }
+    }
+
+    protected void chaserTransition(Event event) {
+        if (event != null && EventType.PAC_MAN_SUPER_SUGAR_COLLISION.equals(event.getType())) {
+            chaser.pause();
+            frightened.enter(this);
+
+            previousMode = chaser;
+            activeMode = frightened;
+
+            final float activePeriodSec = activeMode.getActivePeriodSeconds();
+            realTimer.start(activePeriodSec);
+        } else if (chaser.ended()) {
+            chaser.exit();
+            scattered.enter();
+
+            activeMode = scattered;
+        }
+    }
+    protected void scatteredTransition(Event event) {
+        if (event != null && EventType.PAC_MAN_SUPER_SUGAR_COLLISION.equals(event.getType())) {
+            scattered.pause();
+            frightened.enter(this);
+
+            previousMode = scattered;
+            activeMode = frightened;
+
+            final float activePeriodSec = activeMode.getActivePeriodSeconds();
+            realTimer.start(activePeriodSec);
+        } else if (scattered.ended()) {
+            scattered.exit();
+            chaser.enter();
+
+            activeMode = chaser;
+        }
+    }
+
+    protected void frightenedTransition(Event event) {
+        if (event != null && EventType.PAC_MAN_GHOST_COLLISION.equals(event.getType())) {
+            eaten.enter(this);
+            activeMode = eaten;
+        } else if (realTimer.up()) {
+            previousMode.enter();
+            activeMode = previousMode;
+            previousMode = null;
+        }
+    }
+
+    protected void eatenTransition(GhostMode eaten) {
+        if (eaten.end(this)) {
+            activeMode = previousMode;
+            activeMode.enter(this);
+        }
+    }
 }
