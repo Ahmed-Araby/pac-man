4205d8666bc20b7ba706a660b04bc4bd1806a66d
Ahmed Araby <ahmedaraby605@gmail.com>
2026-05-30 14:03:08 +0300

    remove the unused method M2MSpriteCollisionDetector.outOfCanvas

--- src/main/java/com/ahmedaraby/jengine/collision/sprite/M2MSpriteCollisionDetector.java
+++ src/main/java/com/ahmedaraby/jengine/collision/sprite/M2MSpriteCollisionDetector.java
@@ -2,7 +2,6 @@ package com.ahmedaraby.jengine.collision.sprite;
 
 
 import com.ahmedaraby.jengine.collision.geometric.Rect2RectCollisionDetectorUtil;
-import com.ahmedaraby.game.pacman.constant.DimensionsC;
 import com.ahmedaraby.jengine.entity.Rectangle;
 import com.ahmedaraby.jengine.models.CollisionReport;
 
@@ -14,11 +13,5 @@ public class M2MSpriteCollisionDetector {
     public static Optional<CollisionReport> detect(Rectangle rectA, Rectangle rectB) {
         final Boolean collide = Rect2RectCollisionDetectorUtil.collide(rectA, rectB);
         return collide ? Optional.of(new CollisionReport(rectA, List.of(rectB))) : Optional.empty();
-    }
-
-    // [TODO] remove this method
-    private static boolean outOfCanvas(Rectangle rect) {
-        return rect.topEdgeRow() < 0 || rect.bottomEdgeRow() >= DimensionsC.CANVAS_HEIGHT_PIXELS
-                || rect.leftEdgeCol() < 0 || rect.rightEdgeCol() >= DimensionsC.CANVAS_WIDTH_PIXELS;
     }
 }
