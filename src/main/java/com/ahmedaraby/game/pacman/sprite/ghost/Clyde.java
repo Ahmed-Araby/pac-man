757f73bf5bcbfa9a0cea6bde303d2a1cc95e2a27
Ahmed Araby <ahmedaraby605@gmail.com>
2026-07-03 16:29:00 +0300

    treat the Clyde's ScaredChaser mode as sub mode of Chaser mode

--- src/main/java/com/ahmedaraby/game/pacman/sprite/ghost/Clyde.java
+++ src/main/java/com/ahmedaraby/game/pacman/sprite/ghost/Clyde.java
@@ -89,6 +89,8 @@ public class Clyde extends Ghost {
             // [TODO] provide the number 8 as a configuration
             if (distToPacManInPixels >= 8 * configs.PLAYGROUND_CELL_SIZE()) {
                 activeMode = chaser;
+            } else if (chaser.ended()) {
+                super.chaserTransition(event);
             }
         }
     }
