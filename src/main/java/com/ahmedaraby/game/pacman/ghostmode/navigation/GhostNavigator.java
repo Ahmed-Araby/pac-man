4973b3e4db7ccf0bd88e8e0dfdd4445b553bdf03
Ahmed Araby <ahmedaraby605@gmail.com>
2026-07-23 14:29:48 +0300

    remove unused methods

--- src/main/java/com/ahmedaraby/game/pacman/ghostmode/navigation/GhostNavigator.java
+++ src/main/java/com/ahmedaraby/game/pacman/ghostmode/navigation/GhostNavigator.java
@@ -3,13 +3,9 @@ package com.ahmedaraby.game.pacman.ghostmode.navigation;
 import com.ahmedaraby.game.pacman.entity.Cell;
 import com.ahmedaraby.game.pacman.sprite.MovingSprite;
 import com.ahmedaraby.game.pacman.sprite.Sprite;
-import com.ahmedaraby.jengine.entity.Coordinate;
-import com.ahmedaraby.jengine.entity.Vector;
 
 public interface GhostNavigator {
 
-    Vector calcDir(MovingSprite sprite, Coordinate target);
     double calcDist(MovingSprite sprite, Sprite targetSprite);
-    double calcDist(MovingSprite sprite, Coordinate target);
     double calcDist(Cell source, Cell target);
 }
--- src/main/java/com/ahmedaraby/game/pacman/ghostmode/navigation/ShortestPathNavigator.java
+++ src/main/java/com/ahmedaraby/game/pacman/ghostmode/navigation/ShortestPathNavigator.java
@@ -1,22 +1,11 @@
 package com.ahmedaraby.game.pacman.ghostmode.navigation;
 
-import com.ahmedaraby.game.pacman.collision.M2SSpriteCollisionDetector;
 import com.ahmedaraby.game.pacman.config.intConfigs.ConfigsEx;
-import com.ahmedaraby.game.pacman.constant.SpriteE;
 import com.ahmedaraby.game.pacman.entity.Cell;
-import com.ahmedaraby.game.pacman.entity.MovementPlan;
-import com.ahmedaraby.game.pacman.playground.Playground;
 import com.ahmedaraby.game.pacman.sprite.MovingSprite;
 import com.ahmedaraby.game.pacman.sprite.Sprite;
-import com.ahmedaraby.jengine.entity.Rectangle;
-import com.ahmedaraby.jengine.entity.Vector;
 import lombok.AllArgsConstructor;
-import com.ahmedaraby.jengine.entity.Coordinate;
-import com.ahmedaraby.game.pacman.entity.MazeMove;
 import com.ahmedaraby.game.pacman.util.PlaygroundShortestPathNav;
-
-import java.util.List;
-import java.util.Objects;
 
 
 @AllArgsConstructor
@@ -32,13 +21,6 @@ public class ShortestPathNavigator implements GhostNavigator {
         return playgroundShortestPathNav.calcDist(sourceCell, targetCell) * configs.PLAYGROUND_CELL_SIZE();
     }
 
-    @Override
-    public double calcDist(MovingSprite movingSprite, Coordinate target) {
-        Cell sourceCell = movingSprite.calcCell();
-        Cell targetCell = target.toCell();
-        return playgroundShortestPathNav.calcDist(sourceCell, targetCell) * configs.PLAYGROUND_CELL_SIZE();
-    }
-
     /**
      *
      * @param source
@@ -48,59 +30,5 @@ public class ShortestPathNavigator implements GhostNavigator {
     @Override
     public double calcDist(Cell source, Cell target) {
         return playgroundShortestPathNav.calcDist(source, target);
-    }
-
-    // [TODO] delete
-    @Override
-    public Vector calcDir(MovingSprite sprite, Coordinate target) {
-        final Coordinate source = sprite.getTopLeftCorner();
-        if(source.equals(target)) {
-            return Vector.STILL;
-        }
-
-        final List<MovementPlan> possibleMoves = getCandidateMoves(sprite, target);
-        return possibleMoves
-                .stream()
-                .sorted()
-                .filter(plan -> plan.getDist2Target() < Integer.MAX_VALUE)
-                .map(MovementPlan::getDir)
-                .findFirst()
-                .orElse(Vector.STILL);
-    }
-
-
-    private List<MovementPlan> getCandidateMoves(MovingSprite moving, Coordinate target) {
-        // this work can be parallelized
-        final Cell targetCell = target.toCell();
-        final List<MazeMove> candidateNextCell = getCandidateNextMoves(moving);
-        return candidateNextCell
-                .stream()
-                // make sure that this move won't cause a collision
-                .filter(move -> {
-                    final Coordinate nextCord = moving.calcNextCord(moving.calcHEmptySpaceInPlaygroundCell(), move.getDir());
-                    final Rectangle nextRect = new Rectangle(nextCord, moving.getWidth(), moving.getHeight());
-                    return M2SSpriteCollisionDetector.detect(nextRect, List.of(SpriteE.WALL, SpriteE.GHOST_HOUSE_WALL)).isEmpty();
-                })
-                .map(move -> {
-                    final int dist = playgroundShortestPathNav.calcDist(move.getTo(), targetCell);
-                    if (dist == Integer.MAX_VALUE) {
-                        return null;
-                    }
-                    return new MovementPlan(move.getDir(), dist);
-                })
-                .filter(Objects::nonNull)
-                .toList();
-    }
-
-
-    private List<MazeMove> getCandidateNextMoves(MovingSprite sprite) {
-        Cell spriteCell = sprite.getTopLeftCorner().toCell();
-        List<MazeMove> candidateNextCells = spriteCell.getMoves(configs.PLAYGROUND_WIDTH(), configs.PLAYGROUND_HEIGHT());
-
-        return candidateNextCells
-                .stream()
-                .filter(move -> !move.getDir().isOpposite(sprite.getDirV()))
-                .filter(move -> !Playground.isWall(move.getTo()) && !Playground.isGhostHWall(move.getTo()))
-                .toList();
     }
 }
