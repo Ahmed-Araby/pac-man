de422c6344659ce91a2a7ce37b708475c165a358
Ahmed Araby <ahmedaraby605@gmail.com>
2026-07-23 11:42:10 +0300

    render and move Blinky, Inky and Pinky

--- src/main/java/com/ahmedaraby/game/pacman/scene/GamePlayGameScene.java
+++ src/main/java/com/ahmedaraby/game/pacman/scene/GamePlayGameScene.java
@@ -202,9 +202,9 @@ public class GamePlayGameScene implements GameScene {
         }
 
         pacMan.render(canvas);
-//        blinky.render(canvas);
-//        inky.render(canvas);
-//        pinky.render(canvas);
+        blinky.render(canvas);
+        inky.render(canvas);
+        pinky.render(canvas);
         clyde.render(canvas);
 
         gameState.setPrevFrameEndedAt(System.nanoTime());
@@ -215,9 +215,9 @@ public class GamePlayGameScene implements GameScene {
         gameState.setCurrFrameStartedAt(System.nanoTime());
 
         pacMan.move(null);
-//        blinky.move(null);
-//        inky.move(null);
-//        pinky.move(null);
+        blinky.move(null);
+        inky.move(null);
+        pinky.move(null);
         clyde.move(null);
 
         collisionSystem.detect();
