d185f6b3695e68aeb31182edbd622abacb635e6d
Ahmed Araby <ahmedaraby605@gmail.com>
2026-07-25 00:02:44 +0300

    move the method isPossibleMove(MazeMove) to the MachineControlledSprite abstract class

--- src/main/java/com/ahmedaraby/game/pacman/sprite/MachineControlledSprite.java
+++ src/main/java/com/ahmedaraby/game/pacman/sprite/MachineControlledSprite.java
@@ -55,6 +55,43 @@ public abstract class MachineControlledSprite extends MovingSprite {
         return moves;
     }
 
+    /**
+     * first, it checks if the destination cell is a valid cell for the sprite to move to, i.e. it is not out of canvas
+     * neither it is a wall or a ghost house wall.
+     * <p>
+     * second, it checks if the sprite is actually capable of moving to this cell by applying the calibrated stride
+     * and make sure that the hypothetical sprite (i.e. after applying the move) is not going out of canvas
+     * neither colliding.
+     * <p>
+     * you might wander, why in earth the first and second check has to be made, either one of them on its own is enough,
+     * well,
+     * <p>
+     * the second check is needed because the sprite is almost never perfectly aligned with the cell it is trying to move to,
+     * so, the cell might be a valid cell but the sprite can't do the move at the right moment because if it does, it will
+     * collide with a wall. instead the sprite might need to move in the current direction in order to near perfect align
+     * with the next cell and do the move without collision.
+     * <p>
+     * and the first part is needed to prevent the sprite from moving on the sub pixel level (using small calibrated strides)
+     * towards a cell that will eventually appear to be not a possible goal, without the first check the sprites might look
+     * like they are stuck for a while, by repeatedly applying tiny strides.
+     *
+     * @param move an object that contains the cell to which the sprite is trying to move (to) and the movement direction
+     *             relative to the current direction of the sprite.
+     * @return true if the movement is possible.
+     */
+    protected boolean isPossibleMove(MazeMove move) {
+        if (Playground.isWall(move.getTo())
+                || Playground.isGhostHWall(move.getTo())
+                || Vector.STILL.equals(move.getDir())) {
+            return false;
+        }
+        if (move.getDir().equals(dirV)) {
+            // [TODO] should consider opposite direction, (i.e. any direction parallel to the current direction)
+            return isPossibleToMoveInSameDir();
+        }
+        return isPossibleToMoveInNewDir(move.getDir());
+    }
+
     @Override
     protected boolean isPossibleMove(double stride, Vector dir) {
         // [TODO] make this threshold be configured
